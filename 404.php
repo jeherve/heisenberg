d7e373064a09d439f7eaf1171e6104fb252648de
Jeremy Herve <jeremy@tagada.hu>
2013-02-19 21:57:21 +0100

    Remove part of the 404 template

--- 404.php
+++ 404.php
@@ -27,12 +27,6 @@ get_header(); ?>
 						</ul>
 					</div><!-- .widget -->
 
-					<?php
-					/* translators: %1$s: smiley */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'heisenberg' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
 					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
 				</div><!-- .entry-content -->
