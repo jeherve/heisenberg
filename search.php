f118181ba5a6b018c10b01879a005d0569090fa7
Jeremy Herve <jeremy@tagada.hu>
2013-02-19 15:28:43 +0100

    Remove nav-above

--- search.php
+++ search.php
@@ -14,8 +14,6 @@ get_header(); ?>
 					<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'heisenberg' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 				</header><!-- .page-header -->
 
-				<?php heisenberg_content_nav( 'nav-above' ); ?>
-
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
