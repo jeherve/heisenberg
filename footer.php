d8e4601a47daee036cc3cc4f2b473f606bb373ed
Jeremy Herve <jeremy@tagada.hu>
2013-02-19 09:45:20 +0100

    Initial commit. Let the Hack Day begin!

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package Heisenberg
+ * @since Heisenberg 1.0
+ */
+?>
+
+	</div><!-- #main .site-main -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<?php do_action( 'heisenberg_credits' ); ?>
+			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'heisenberg' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'heisenberg' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'heisenberg' ), 'Heisenberg', '<a href="http://jeremyherve.com/" rel="designer">Jeremy Herve</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon .site-footer -->
+</div><!-- #page .hfeed .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ inc/customizer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Heisenberg Theme Customizer
+ *
+ * @package Heisenberg
+ * @since Heisenberg 1.2
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ *
+ * @since Heisenberg 1.2
+ */
+function heisenberg_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport        = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'heisenberg_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ *
+ * @since Heisenberg 1.2
+ */
+function heisenberg_customize_preview_js() {
+	wp_enqueue_script( 'heisenberg_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20120827', true );
+}
+add_action( 'customize_preview_init', 'heisenberg_customize_preview_js' );
